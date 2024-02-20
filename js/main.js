27f703d59fd1e013c1fda88ed3f20f3e8f023a56
Kento Uchida <21z1016@stu.meisei-u.ac.jp>
2024-02-20 14:05:25 +0900

    add responsive

--- js/main.js
+++ js/main.js
@@ -2,5 +2,22 @@ $('.slider').slick({
   dots: true,
   infinite: true,
   slidesToShow: 3,
-  slidesToScroll: 3
+  slidesToScroll: 3,
+
+  responsive: [
+    {
+      breakpoint: 769,//モニターの横幅が769px以下の見せ方
+      settings: {
+        slidesToShow: 2,//スライドを画面に2枚見せる
+        slidesToScroll: 2,//1回のスクロールで2枚の写真を移動して見せる
+      }
+    },
+    {
+      breakpoint: 426,//モニターの横幅が426px以下の見せ方
+      settings: {
+        slidesToShow: 1,//スライドを画面に1枚見せる
+        slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
+      }
+    }
+  ]
 });
